a455d9dc1ccf581f995de454d9cdf6e9da4d6074
Allen Zhu <asz241@nyu.edu>
2019-07-18 17:41:51 -0400

    New Calc

--- calculator.py
+++ calculator.py
@@ -2,17 +2,23 @@ num1 = float(input("Enter in your first number: "))
 op = input("Enter your operator: ")
 num2 = float(input("Enter in your second number: "))
 
-if op = "+"
-    print (num1+num2)
-elif op = "-"
-    print (num1 - num2)
-elif op = "*"
-    print (num1 * num2)
-elif op = "/"
-    print (num1 / num2)
-if result != None
-    print('{}{}{} ={}', format(
-    num1,
-    operator,
-    num2,
-    result))
+
+result = None
+if op == "+":
+    result = (num1+num2)
+    print(result)
+elif op == "-":
+    result = (num1 - num2)
+    print(result)
+elif op == "*":
+    result =(num1 * num2)
+    print(result)
+elif op == "/":
+    result = (num1 / num2)
+    print(result)
+else:
+    print("Error")
+
+if result != None:
+    print('{}{}{} ={}' .format(num1, op, num2, result))
+    # print("User input error. Please double check to makes sure that your operator is correct")
