c0d2e4a4e1f38b06a496e2a9ec06041b4216c2ff
Allen Zhu <asz241@nyu.edu>
2019-07-18 16:57:41 -0400

    Added Calculator Code

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter in your first number: "))
+op = input("Enter your operator: ")
+num2 = float(input("Enter in your second number: "))
+
+if op = "+"
+    print (num1+num2)
+elif op = "-"
+    print (num1 - num2)
+elif op = "*"
+    print (num1 * num2)
+elif op = "/"
+    print (num1 / num2)
+if result != None
+    print('{}{}{} ={}', format(
+    num1,
+    operator,
+    num2,
+    result))
